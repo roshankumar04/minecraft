841be45e96ca68de0ac0dbdda7a90b41b3d48071
roshankumar04 <90085148+roshankumar04@users.noreply.github.com>
2022-05-14 15:38:38 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var canvas=new fabric.Canvas("myCanvas");
-block_width=30;
-block_height=30;
+block_image_width=30;
+block_image_height=30;
 player_x=10;
 player_y=10;
 
@@ -11,7 +11,7 @@ function player_update(){
     fabric.Image.fromURL("player.png",function(Img){
         player_object=Img;
         player_object.scaleToWidth(150);
-        player_object.scaleToheight(140);
+        player_object.scaleToHeight(140);
         player_object.set({
 top:player_y,
 left:player_x 
@@ -25,7 +25,7 @@ function new_image(get_image){
     fabric.Image.fromURL(get_image,function(Img){
         block_image_object=Img;
         block_image_object.scaleToWidth(block_width);
-        block_image_object.scaleToheight(block_height);
+        block_image_object.scaleToHeight(block_height);
         block_image_object.set({
 top:player_y,
 left:player_x 
